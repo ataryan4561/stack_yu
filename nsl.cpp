4daa641267ded0ade216adb2d11b089a39435b50
ataryan4561 <ataryan4561@gmail.com>
2021-08-19 18:59:51 +0530

    first commit

---
+++ nsl.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#include<stack>
+using namespace std;
+int main()
+{
+    int arr[4]={1,3,2,4};
+    stack<int> st;
+    vector<int> ans;
+    for(int i=0; i<4; i++)
+    {
+        if(st.size()==0)
+        {
+            ans.push_back(-1);
+        }
+        else if(st.size()>0 && st.top()<arr[i])
+        {
+            ans.push_back(st.top());
+        }
+        else if(st.size()>0 && st.top()>=arr[i])
+        {
+            while(st.size()>0 && st.top()>=arr[i])
+            {
+                st.pop();
+            }
+            if(st.size()==0)
+            {
+                ans.push_back(-1);
+            }
+            else if(st.top()<arr[i])
+            {
+                ans.push_back(st.top());
+            }
+        }
+        st.push(arr[i]);
+    }
+    for(int i=0; i<4; i++)
+    {
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+}
---
+++ nsr.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+#include<stack>
+using namespace std;
+int main()
+{
+    int arr[4]={1,2,1,4};
+    vector<int> ans;
+    stack<int> st;
+    for(int i=3; i>=0; i--)
+    {
+        if(st.size()==0)
+        {
+            ans.push_back(-1);
+        }
+        else if(st.size()>0 && st.top()<arr[i])
+        {
+            ans.push_back(st.top());
+        }
+        else if(st.size()>0 && st.top()>=arr[i])
+        {
+            while(st.size()>0 && st.top()>=arr[i])
+            {
+                st.pop();
+            }
+            if(st.size()==0)
+            {
+                ans.push_back(-1);
+            }
+            else if(st.top()<arr[i])
+            {
+                ans.push_back(st.top());
+            }
+        }
+        st.push(arr[i]);
+    }
+    for(int i=ans.size()-1; i>=0; i--)
+    {
+        cout<<ans[i]<<" ";
+    }
+    cout<<endl;
+}
